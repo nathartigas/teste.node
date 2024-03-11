7e93cbc594cf5d0d7b426111e072496d14247b0f
nathartigas <nathartigasna@gmail.com>
2024-03-11 15:05:05 -0300

    subir repositorio

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('olá mundo');
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+
+console.log(chalk.blue.bgWhite.bold('SENAI'));
+
+console.log(chalk.blue('curso','de','Node.js'));
+
+console.log(chalk.red('vermelho',chalk.underline.bgBlue('azul')));
+
+console.log(
+    `CPU: ${chalk.red('90%')}
+    RAM: ${chalk.green('40%')}
+    DISK: ${chalk.yellow('70%')}`
+);
